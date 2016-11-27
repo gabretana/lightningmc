34e0797644a01b8957c4d11250335ef8b0c23f8b
Gabriel Retana <gabrielretana@about.me>
2016-11-27 12:48:32 -0600

    Update application version to 0.7.2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("LightningMC");
-    a.setApplicationVersion("0.3.1");
+    a.setApplicationVersion("0.7.2");
     a.setOrganizationName("GXA Software");
     MainWindow w;
     w.show();
